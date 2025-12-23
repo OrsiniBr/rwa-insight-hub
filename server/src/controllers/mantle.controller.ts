6b97a635dbde91ef124e70264fb31f240b1c4e55
Biokes <106444765+Biokes@users.noreply.github.com>
2025-12-23 08:08:14 +0100

    update[server_update]: refactor controller for responses and clear error messaged

--- server/src/controllers/mantle.controller.ts
+++ server/src/controllers/mantle.controller.ts
@@ -1,6 +1,7 @@
 import mantleService from "../services/mantle.service";
+import ApiResponse from "../utils/apiResponse";
 
-export const getMantleTokens = async (req, res) => {
+export const getMantleTokens = async (_req, res) => {
     try {
         let tokens = await mantleService.getTokensFromDB();
         if (!tokens.length) {
@@ -8,8 +9,8 @@ export const getMantleTokens = async (req, res) => {
             await mantleService.saveTokens(fetchedTokens);
             tokens = await mantleService.getTokensFromDB();
         }
-        res.json(ApiResponse());
+        return ApiResponse.success(res,tokens,200);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        return ApiResponse.error(res,400,error,error instanceof Error? error.message: "Something went wrong");
     }
 };
