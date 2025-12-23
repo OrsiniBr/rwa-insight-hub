4055cd1c3dca18ea54d16eb3fa926089e0e1b931
OrsiniBr <udochukwugodwin13@gmail.com>
2025-12-23 23:49:09 +0100

    remove mantle mainnet

--- client/src/rainbowKitConfig.tsx
+++ client/src/rainbowKitConfig.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mantle, mantleTestnet, mantleSepoliaTestnet, base } from "viem/chains";
+import {mantleSepoliaTestnet, base } from "viem/chains";
 
 export default getDefaultConfig({
   appName: "Cross-Credit Lending",
   projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
-  chains: [mantle, mantleTestnet, mantleSepoliaTestnet, base],
+  chains: [mantleSepoliaTestnet, base],
   ssr: false,
 });
